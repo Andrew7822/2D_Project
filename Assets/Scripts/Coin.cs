cc5d94f0aa81a17ebd4472b96b92eea1a885191c
Kellani <lilkenjoi@gmail.com>
2024-11-09 19:48:19 +0300

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private float _sleepTime;
+
+    private SpriteRenderer _spriteRenderer;
+    private Collider2D _collider;
+
+    private WaitForSeconds _sleepTimer;
+
+    private void Awake()
+    {
+        _sleepTimer = new WaitForSeconds(_sleepTime);
+         _collider = GetComponent<Collider2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(Respawning());
+    }
+
+    private IEnumerator Respawning()
+    {
+        _collider.enabled = false;
+        _spriteRenderer.enabled = false;
+
+        yield return _sleepTimer;
+
+        _spriteRenderer.enabled = true;
+        _collider.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private const string Horizontal = "Horizontal";
+    private const string Jump = "Jump";
+
+    [SerializeField] private Transform _grooundDetector;
+
+    private Rigidbody2D _rigidbody2D;
+    private Animators _animators;
+    private Animator _animator;
+
+    private int _speed = 3;
+    private float _jumpForce = 0.5f;
+    private bool _isGrounded;
+
+    Mover _mover;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animators = new Animators(_animator);
+        _mover =  new Mover();
+    }
+
+    private void Update()
+    {
+        ExploreGround();
+
+        _animators.EnableAnimationIdle();
+
+        if (Input.GetButton(Horizontal))
+        {
+            _mover.Run(transform, _speed, Horizontal);
+            _animators.EnableAnimationRun();
+        }
+
+        if (Input.GetButton(Jump))
+        {
+            _mover.Jump(_rigidbody2D, transform, _jumpForce, _isGrounded);
+            _animators.EnableAnimationJump();
+        }
+    }
+
+    public void ExploreGround()
+    {
+        float radius = 0.1f;
+        int arrayLength = 1;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_grooundDetector.position, radius);
+        _isGrounded = colliders.Length > arrayLength;
+    }
+}
+
+public class Mover
+{
+    private readonly int degreesRotation = 180;
+
+    public void Run(Transform transform, int speed, string Horizontal)
+    {
+        if (Input.GetAxis(Horizontal) > 0)
+        {
+            Flip(0, transform);
+        }
+
+        if (Input.GetAxis(Horizontal) < 0)
+        {
+            Flip(degreesRotation, transform);
+        }
+
+        Vector3 directionVector = Vector3.right * Input.GetAxisRaw(Horizontal);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + directionVector, speed * Time.deltaTime);
+    }
+
+    public void Jump(Rigidbody2D rigidbody2D, Transform transform, float jumpForce, bool isGrounded)
+    {
+        if (isGrounded)
+        {
+            rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Flip(int degreesRotation, Transform transform)
+    {
+        Quaternion _rotation = transform.rotation;
+
+        _rotation.y = degreesRotation;
+        transform.rotation = _rotation;
+    }
+}
